d781340a639fe31dd29b31b0a9a14fcf418f5d2b
Raúl Donoso <rauld08@hotmail.com>
2023-04-19 19:08:08 -0400

    Commit inicial

---
+++ assets/js/script.js
@@ -0,0 +1,75 @@
+$(document).ready(function(){
+
+    $("#proy1 .tarjeta").hover(function(){
+        $("#proy1 .imgTarjeta").addClass("imgTarjetaHover")
+        $("#proy1 .textoTarjeta").addClass("textoTarjetaHover")
+        $("#proy1 .tituloTarjeta").addClass("tituloTarjetaHover")
+        $("#proy1 .btnTarjeta").addClass("btnTarjetaHover")
+    },function(){
+        $("#proy1 .imgTarjeta").removeClass("imgTarjetaHover")
+        $("#proy1 .textoTarjeta").removeClass("textoTarjetaHover")
+        $("#proy1 .tituloTarjeta").removeClass("tituloTarjetaHover")
+        $("#proy1 .btnTarjeta").removeClass("btnTarjetaHover")
+    })
+    
+    $("#proy2 .tarjeta").hover(function(){
+        $("#proy2 .imgTarjeta").addClass("imgTarjetaHover")
+        $("#proy2 .textoTarjeta").addClass("textoTarjetaHover")
+        $("#proy2 .tituloTarjeta").addClass("tituloTarjetaHover")
+        $("#proy2 .btnTarjeta").addClass("btnTarjetaHover")
+    },function(){
+        $("#proy2 .imgTarjeta").removeClass("imgTarjetaHover")
+        $("#proy2 .textoTarjeta").removeClass("textoTarjetaHover")
+        $("#proy2 .tituloTarjeta").removeClass("tituloTarjetaHover")
+        $("#proy2 .btnTarjeta").removeClass("btnTarjetaHover")
+    })
+
+    $("#proy3 .tarjeta").hover(function(){
+        $("#proy3 .imgTarjeta").addClass("imgTarjetaHover")
+        $("#proy3 .textoTarjeta").addClass("textoTarjetaHover")
+        $("#proy3 .tituloTarjeta").addClass("tituloTarjetaHover")
+        $("#proy3 .btnTarjeta").addClass("btnTarjetaHover")
+    },function(){
+        $("#proy3 .imgTarjeta").removeClass("imgTarjetaHover")
+        $("#proy3 .textoTarjeta").removeClass("textoTarjetaHover")
+        $("#proy3 .tituloTarjeta").removeClass("tituloTarjetaHover")
+        $("#proy3 .btnTarjeta").removeClass("btnTarjetaHover")
+    })
+
+    $("#proy4 .tarjeta").hover(function(){
+        $("#proy4 .imgTarjeta").addClass("imgTarjetaHover")
+        $("#proy4 .textoTarjeta").addClass("textoTarjetaHover")
+        $("#proy4 .tituloTarjeta").addClass("tituloTarjetaHover")
+        $("#proy4 .btnTarjeta").addClass("btnTarjetaHover")
+    },function(){
+        $("#proy4 .imgTarjeta").removeClass("imgTarjetaHover")
+        $("#proy4 .textoTarjeta").removeClass("textoTarjetaHover")
+        $("#proy4 .tituloTarjeta").removeClass("tituloTarjetaHover")
+        $("#proy4 .btnTarjeta").removeClass("btnTarjetaHover")
+    })
+
+    $("#proy5 .tarjeta").hover(function(){
+        $("#proy5 .imgTarjeta").addClass("imgTarjetaHover")
+        $("#proy5 .textoTarjeta").addClass("textoTarjetaHover")
+        $("#proy5 .tituloTarjeta").addClass("tituloTarjetaHover")
+        $("#proy5 .btnTarjeta").addClass("btnTarjetaHover")
+    },function(){
+        $("#proy5 .imgTarjeta").removeClass("imgTarjetaHover")
+        $("#proy5 .textoTarjeta").removeClass("textoTarjetaHover")
+        $("#proy5 .tituloTarjeta").removeClass("tituloTarjetaHover")
+        $("#proy5 .btnTarjeta").removeClass("btnTarjetaHover")
+    })
+
+    $("#proy6 .tarjeta").hover(function(){
+        $("#proy6 .imgTarjeta").addClass("imgTarjetaHover")
+        $("#proy6 .textoTarjeta").addClass("textoTarjetaHover")
+        $("#proy6 .tituloTarjeta").addClass("tituloTarjetaHover")
+        $("#proy6 .btnTarjeta").addClass("btnTarjetaHover")
+    },function(){
+        $("#proy6 .imgTarjeta").removeClass("imgTarjetaHover")
+        $("#proy6 .textoTarjeta").removeClass("textoTarjetaHover")
+        $("#proy6 .tituloTarjeta").removeClass("tituloTarjetaHover")
+        $("#proy6 .btnTarjeta").removeClass("btnTarjetaHover")
+    })
+    
+})
